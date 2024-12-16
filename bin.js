ca55aba100c3dca549efc61825178b79f17928f0
asdqdsa <asdqdsa@protonmail.com>
2024-12-16 14:01:06 +0400

    luck: add more luck

--- bin.js
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env node
-console.log("\x1b[35m%s\x1b[0m", "luck buff: ✨ your luck has increased! +1");
--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("\x1b[35m%s\x1b[0m", "luck buff: ✨ your luck has increased! +1");
+#!/usr/bin/env node
+const luckMessages = [
+  "your luck has increased! ✨",
+  "fortune smiles upon you today! 🍀",
+  "you've unlocked a +1 luck buff! 🎲",
+  "your luck just leveled up! 💫",
+  "your luck is off the charts! 🌟",
+  "luck buff: ✨ your luck has increased! +1"
+];
+
+const randomMessage = luckMessages[Math.floor(Math.random() * luckMessages.length)];
+console.log("\x1b[35m%s\x1b[0m", randomMessage);
